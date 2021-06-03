5259684ec97f8e62dd5da4d10140c97ed0fe885e
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-06-03 13:40:56 +0200

    Add images to sequence

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -16,14 +16,20 @@ class VideoDataset(Dataset):
 
     def __getitem__(self, idx):
         data_point = self.data[idx]
-        sequence = self.feature_to_sequence(torch.FloatTensor(data_point['sequence']))
-        label = torch.FloatTensor([data_point['label']])
+        images = self.video.get_frames(data_point['video'])
+        sequence = self.feature_to_sequence(torch.FloatTensor(data_point['sequence']), images[:-1])
+        label = torch.cat((torch.FloatTensor([data_point['label']]), images[-1].flatten(1)), 1)
         return {
+            'sequence': sequence,
             'label': label,
         }
 
-    def feature_to_sequence(self, feature):
+    def feature_to_sequence(self, feature, images):
+        images = images.flatten(1)
+        # time descending, last image -> first image
+        images = torch.cat((images[-1].repeat(20, 1), images[0].repeat(20, 1)))
+
         gazes = torch.reshape(feature[:80], (40, 2))
         head = torch.reshape(feature[80:160], (40, 2))
         task = torch.reshape(feature[160: 640], (40, 12))
-        return torch.cat((gazes, head, task), 1)
+        return torch.cat((gazes, head, task, images), 1)
