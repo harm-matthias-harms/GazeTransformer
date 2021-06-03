0628605956eee8153ea3942af49162e53abdf040
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-06-03 13:38:56 +0200

    Add cropping to generation script

--- dataloader/generate_saliency_video.py
+++ dataloader/generate_saliency_video.py
@@ -4,6 +4,7 @@ import numpy as np
 import decord
 import progressbar
 import sys
+import torchvision.transforms.functional as TF
 
 sys.path.append("../model")
 
@@ -12,14 +13,14 @@ from saliency import SaliencyMap
 
 
 GENERATE_PATH = '../dataset/dataset/SaliencyVideos/'
-batch_size = 32
+batch_size = 128
 
 decord.bridge.set_bridge("torch")
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == '__main__':
     filenames = get_filenames()
-    model = SaliencyMap((600, 540)).to(device)
+    model = SaliencyMap((256, 256)).to(device)
     for idx, files in enumerate(filenames):
         print(idx)
         result = []
@@ -28,14 +29,15 @@ if __name__ == '__main__':
         video_length = len(video)
         filename = GENERATE_PATH + files[0].split("bandicam ")[-1]
         out = cv2.VideoWriter(
-            filename, cv2.VideoWriter_fourcc(*'mp4v'), 60, (24, 24), 0)
+            filename, cv2.VideoWriter_fourcc(*'XVID'), 60, (24, 24), 0)
 
         batches = [list(range(video_length))[i * batch_size:(i + 1) * batch_size]
                    for i in range((video_length + batch_size - 1) // batch_size)]
         for batch in progressbar.progressbar(batches):
             images = video.get_batch(batch).float()
-            images /= 255.0
             images = images.permute(0, 3, 1, 2)
+            images = TF.center_crop(images, (256, 256))
+            images /= 255.0
             pred = model(images)
             pred = pred.permute(0, 2, 3, 1)
             pred = pred.cpu().numpy()
