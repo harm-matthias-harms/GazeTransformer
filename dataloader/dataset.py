e03fb065c1a8e47d07a89f05c1daf4816051bc0e
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-06-10 10:47:56 +0200

    Add dataset for fixationnet modal

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -5,6 +5,38 @@ from torch.utils.data import Dataset
 from .video import VideoParser, SaliencyVideoParser
 
 
+class TimeSequenceVideoDataset(Dataset):
+    def __init__(self, video_path, sequence_path, base_timestamp, start_timestamp, is_saliency=False):
+        if is_saliency:
+            self.video = SaliencyVideoParser(
+                video_path, base_timestamp, start_timestamp)
+        else:
+            self.video = VideoParser(
+                video_path, base_timestamp, start_timestamp)
+        with open(sequence_path, 'rb') as f:
+            self.data = dill.load(f)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data_point = self.data[idx]
+        images = self.video.get_frames(data_point['video'])
+        sequence = self.feature_to_sequence(
+            torch.FloatTensor(data_point['sequence']), images[:-1])
+        label = torch.FloatTensor([data_point['label'][:2]])
+        return sequence, label
+
+    def feature_to_sequence(self, feature, images):
+        images = images.flatten(1)
+        # time descending, last image -> first image
+        images = torch.cat((images[-1].repeat(20, 1), images[0].repeat(20, 1)))
+
+        gazes = torch.reshape(feature[:80], (40, 2))
+        head = torch.reshape(feature[80:160], (40, 2))
+        task = torch.reshape(feature[160: 640], (40, 12))
+        return torch.cat((gazes, head, task, images), 1)
+
 class VideoDataset(Dataset):
     def __init__(self, video_path, sequence_path, base_timestamp, start_timestamp, is_saliency=False):
         if is_saliency:
@@ -24,18 +56,22 @@ class VideoDataset(Dataset):
         images = self.video.get_frames(data_point['video'])
         sequence = self.feature_to_sequence(
             torch.FloatTensor(data_point['sequence']), images[:-1])
-        label = torch.cat((torch.FloatTensor([data_point['label']]), images[-1].flatten(1)), 1)#, torch.zeros(1, 8)), 1) #  torch.FloatTensor([data_point['label']])
-        return {
-            'sequence': sequence,
-            'label': label,
-        }
+        label = torch.FloatTensor([data_point['label'][:2]])
+        return sequence, label
 
     def feature_to_sequence(self, feature, images):
         images = images.flatten(1)
         # time descending, last image -> first image
-        images = torch.cat((images[-1].repeat(20, 1), images[0].repeat(20, 1)))
+        images = torch.cat((images[-1], images[0]))
+        return torch.cat((feature, images))
 
-        gazes = torch.reshape(feature[:80], (40, 2))
-        head = torch.reshape(feature[80:160], (40, 2))
-        task = torch.reshape(feature[160: 640], (40, 12))
-        return torch.cat((gazes, head, task, images), 1) # ), 1) # 
+class FeatureDataset(Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, idx):
+        return self.features[idx], self.labels[idx]
--- dataloader/loader.py
+++ dataloader/loader.py
@@ -4,17 +4,19 @@ import torch
 from torch.utils.data import DataLoader, ConcatDataset
 
 from .utility import get_filenames, get_start_timestamps, get_video_timstamps, get_sequence_name, get_saliency_path
-from .dataset import VideoDataset
+from .dataset import TimeSequenceVideoDataset, VideoDataset, FeatureDataset
 
 
-def loadTrainingData(should_train, batch_size, num_workers, sequence_prefix='../dataset/dataset/FixationNet_150_Images/'):
+def loadTrainingData(should_train, batch_size, num_workers, sequence_prefix='../dataset/dataset/FixationNet_150_Images/', as_row=False):
+    dataset_type = VideoDataset if as_row else TimeSequenceVideoDataset
+    
     filenames = get_filenames()
     video_timestamps = get_video_timstamps()
     start_timestamps = get_start_timestamps()
     datasets = []
     for idx, files in enumerate(filenames):
         if should_train[idx]:
-            dataset = VideoDataset(
+            dataset = dataset_type(
                 get_saliency_path(files[0]),
                 os.path.join(os.path.dirname(__file__),
                              sequence_prefix) + get_sequence_name(files[0]),
@@ -28,14 +30,16 @@ def loadTrainingData(should_train, batch_size, num_workers, sequence_prefix='../
     return DataLoader(dataset=concatenated_datasets, batch_size=batch_size, num_workers=num_workers, shuffle=True, drop_last=True)
 
 
-def loadTestData(should_train, batch_size, num_workers, sequence_prefix='../dataset/dataset/FixationNet_150_Images/'):
+def loadTestData(should_train, batch_size, num_workers, sequence_prefix='../dataset/dataset/FixationNet_150_Images/', as_row=False):
+    dataset_type = VideoDataset if as_row else TimeSequenceVideoDataset
+    
     filenames = get_filenames()
     video_timestamps = get_video_timstamps()
     start_timestamps = get_start_timestamps()
     datasets = []
     for idx, files in enumerate(filenames):
         if not should_train[idx]:
-            dataset = VideoDataset(
+            dataset = dataset_type(
                 get_saliency_path(files[0]),
                 os.path.join(os.path.dirname(__file__),
                              sequence_prefix) + get_sequence_name(files[0]),
@@ -47,3 +51,19 @@ def loadTestData(should_train, batch_size, num_workers, sequence_prefix='../data
 
     concatenated_datasets = ConcatDataset(datasets)
     return DataLoader(dataset=concatenated_datasets, batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=True)
+
+def loadOriginalData(batch_size, num_workers):
+    dataset_path = os.path.join(os.path.dirname(__file__), "../dataset/dataset/FixationNet_150_CrossUser/FixationNet_150_User1/")
+    trainingX = torch.from_numpy(np.load(dataset_path + 'trainingX.npy')).float()
+    trainingY = torch.from_numpy(np.load(dataset_path + 'trainingY.npy')).float()
+
+    dataset = FeatureDataset(trainingX, trainingY)
+    return DataLoader(dataset=dataset, batch_size=batch_size, num_workers=num_workers, shuffle=True, drop_last=True)
+
+def loadOriginalTestData(batch_size, num_workers):
+    dataset_path = os.path.join(os.path.dirname(__file__), "../dataset/dataset/FixationNet_150_CrossUser/FixationNet_150_User1/")
+    testX = torch.from_numpy(np.load(dataset_path + 'testX.npy')).float()
+    testY = torch.from_numpy(np.load(dataset_path + 'testY.npy')).float()
+
+    dataset = FeatureDataset(testX, testY)
+    return DataLoader(dataset=dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False, drop_last=True)
