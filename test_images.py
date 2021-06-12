3637d66a63413dbd1bd15f672e2ebd1189ed5b96
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-06-12 20:18:03 +0200

    Remove testing skript

--- test_images.py
+++
@@ -1,9 +0,0 @@
-import torch
-from model.image import FlattenImages
-
-m = FlattenImages()
-
-x = torch.rand(40, 256, 16)
-images = torch.rand(256, 2, 3, 256, 256)
-out = m(x, images)
-print(out.shape)
