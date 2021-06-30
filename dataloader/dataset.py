f9c7f2f3438035446529dd9743c7ed998f96c0e2
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-06-30 14:33:08 +0200

    Fix TimeSequenceVideoDataset

--- dataloader/dataset.py
+++ dataloader/dataset.py
@@ -37,7 +37,7 @@ class TimeSequenceVideoDataset(Dataset):
         gazes = torch.reshape(feature[:80], (40, 2))
         head = torch.reshape(feature[80:160], (40, 2))
         task = torch.reshape(feature[160: 640], (40, 12))
-        if not self.ignore_images and self.in_memory:
+        if not self.ignore_images:
             images = images.flatten(1)
             # time descending, last image -> first image
             images = torch.cat(
