f7674e57b9f5ce6d4e834feea9f94fbc2ee7ff74
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-07-08 11:03:01 +0200

    Add scripts for original no images

---
+++ scripts/original-no-images/final.sh
@@ -0,0 +1,12 @@
+for i in {1..5}; do
+  echo "not implemented: run other versions first"
+  # Users
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 1 -f 'final/user1'
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 2 -f 'final/user2'
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 3 -f 'final/user3'
+  # # Scenes
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 1 -f 'final/scene1'
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 2 -f 'final/scene2'
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 3 -f 'final/scene3'
+  # python ${0%/*}/../../train.py -m original-no-images --delta True -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 4 -f 'final/scene4'
+done
--- scripts/original/head_features.sh
+++ scripts/original/head_features.sh
@@ -1,8 +1,8 @@
 for i in {1..3}; do
   echo "not implemented: run nhead and compress images first"
-  # python ${0%/*}/../../train.py -m original -nh 1 -nl 1 -ih 256 -f 'head_features/256'
-  # python ${0%/*}/../../train.py -m original -nh 1 -nl 1 -ih 128 -f 'head_features/128'
-  # python ${0%/*}/../../train.py -m original -nh 1 -nl 1 -ih 64 -f 'head_features/64'
-  # python ${0%/*}/../../train.py -m original -nh 1 -nl 1 -ih 32 -f 'head_features/32'
-  # python ${0%/*}/../../train.py -m original -nh 1 -nl 1 -ih 16 -f 'head_features/16'
+  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -ih 256 -f 'head_features/256'
+  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -ih 128 -f 'head_features/128'
+  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -ih 64 -f 'head_features/64'
+  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -ih 32 -f 'head_features/32'
+  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -ih 16 -f 'head_features/16'
 done
