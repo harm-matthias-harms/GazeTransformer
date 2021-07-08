5b9ae294f49646bdd02ff0bdea7c88e5f7cdfc4c
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-07-08 16:47:02 +0200

    fix fixationnet evaluation (#20)

--- scripts/fixationnet/generated.sh
+++ scripts/fixationnet/generated.sh
@@ -1,11 +1,11 @@
 for i in {1..5}; do
   # Users
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType user --crossEvalExclude 1 -f 'final/user1'
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType user --crossEvalExclude 2 -f 'final/user2'
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType user --crossEvalExclude 3 -f 'final/user3'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType user --crossEvalExclude 1 -f 'final/user1'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType user --crossEvalExclude 2 -f 'final/user2'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType user --crossEvalExclude 3 -f 'final/user3'
   # Scenes
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType scene --crossEvalExclude 1 -f 'final/scene1'
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType scene --crossEvalExclude 2 -f 'final/scene2'
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType scene --crossEvalExclude 3 -f 'final/scene3'
-  python ${0%/*}/../../train_fixationnet.py -d False --crossEvalType scene --crossEvalExclude 4 -f 'final/scene4'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType scene --crossEvalExclude 1 -f 'final/scene1'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType scene --crossEvalExclude 2 -f 'final/scene2'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType scene --crossEvalExclude 3 -f 'final/scene3'
+  python ${0%/*}/../../train_fixationnet.py -d False --delta True --crossEvalType scene --crossEvalExclude 4 -f 'final/scene4'
 done
