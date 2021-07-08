3ef06b0ce323377280edcf2aa9c9e22b0d51e82f
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-07-08 11:30:04 +0200

    add flatten scripts

---
+++ scripts/flatten/compress_images.sh
@@ -0,0 +1,9 @@
+for i in {1..3}; do
+  echo "not implemented: run nhead first"
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 -f 'compress_images/off'
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 --imageToFeature True --backboneFeatures 16 -f 'compress_images/32'
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 --imageToFeature True --backboneFeatures 32 -f 'compress_images/32'
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 --imageToFeature True --backboneFeatures 64-f 'compress_images/64'
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 --imageToFeature True --backboneFeatures 128 -f 'compress_images/128'
+  # python ${0%/*}/../../train.py -m flatten -nh 1 -nl 1 --imageToFeature True --backboneFeatures 256 -f 'compress_images/256'
+done
