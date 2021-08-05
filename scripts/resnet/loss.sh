4a2940caf47dc5c0f43a2d520ea9fc7de8ea544b
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-08-05 18:06:40 +0200

    Add loss scripts

--- scripts/resnet/loss.sh
+++ scripts/resnet/loss.sh
@@ -1,5 +1,4 @@
-for i in {1..5}; do
-  echo "not implemented: run nhead and head_features first"
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -l angular -nh 1 -nl 1 -f 'loss/angular'
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -l mse -nh 1 -nl 1 -f 'loss/mse'
+for i in {1..2}; do
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -l angular -nh 8 -nl 1 -f 'loss/angular'
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -l mse -nh 8 -nl 1 -f 'loss/mse'
 done
--- scripts/saliency/loss.sh
+++ scripts/saliency/loss.sh
@@ -1,5 +1,4 @@
-for i in {1..5}; do
-  echo "not implemented: run nhead and head_features first"
-  # python ${0%/*}/../../train.py -m saliency -l angular -nh 1 -nl 1 -f 'loss/angular'
-  # python ${0%/*}/../../train.py -m saliency -l mse -nh 1 -nl 1 -f 'loss/mse'
+for i in {1..2}; do
+  python ${0%/*}/../../train.py -m saliency -l angular -nh 2 -nl 1 -f 'loss/angular'
+  python ${0%/*}/../../train.py -m saliency -l mse -nh 2 -nl 1 -f 'loss/mse'
 done
