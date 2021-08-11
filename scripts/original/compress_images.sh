1475e41f108c649aa223712299caac7fb55e8b53
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-08-11 10:35:55 +0200

    Add compress image scripts

--- scripts/original/compress_images.sh
+++ scripts/original/compress_images.sh
@@ -1,9 +1,8 @@
-for i in {1..3}; do
-  echo "not implemented: run nhead first"
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -f 'compress_images/off'
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 16 -f 'compress_images/32'
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 32 -f 'compress_images/32'
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 64-f 'compress_images/64'
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 128 -f 'compress_images/128'
-  # python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 256 -f 'compress_images/256'
+for i in {1..2}; do
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 -f 'compress_images/off'
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 16 -f 'compress_images/32'
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 32 -f 'compress_images/32'
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 64-f 'compress_images/64'
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 128 -f 'compress_images/128'
+  python ${0%/*}/../../train.py -m original --delta True -nh 1 -nl 1 --imageToFeature True --backboneFeatures 256 -f 'compress_images/256'
 done
--- scripts/patches/delta.sh
+++ scripts/patches/delta.sh
@@ -1,4 +1,4 @@
 for i in {1..2}; do
   python ${0%/*}/../../train.py -m patches -nh 6 -nl 1 --delta True -f 'delta/true'
-  python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --delta False -f 'delta/false'
+  python ${0%/*}/../../train.py -m patches -nh 6 -nl 1 --delta False -f 'delta/false'
 done
