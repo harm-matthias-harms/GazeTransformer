2e60af54ffd29328bb17af0732d7697893f4cf83
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-08-19 11:06:33 +0200

    Add head feature training scripts

--- scripts/resnet/head_features.sh
+++ scripts/resnet/head_features.sh
@@ -1,8 +1,7 @@
-for i in {1..3}; do
-  echo "not implemented: run nhead and compress images first"
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 1 -nl 1 -ih 256 -f 'head_features/256'
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 1 -nl 1 -ih 128 -f 'head_features/128'
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 1 -nl 1 -ih 64 -f 'head_features/64'
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 1 -nl 1 -ih 32 -f 'head_features/32'
-  # python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 1 -nl 1 -ih 16 -f 'head_features/16'
+for i in {1..2}; do
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 8 -nl 1 --imageToFeature True --backboneFeatures 128 -ih 256 -f 'head_features/256'
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 8 -nl 1 --imageToFeature True --backboneFeatures 128 -ih 128 -f 'head_features/128'
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 8 -nl 1 --imageToFeature True --backboneFeatures 128 -ih 64 -f 'head_features/64'
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 8 -nl 1 --imageToFeature True --backboneFeatures 128 -ih 32 -f 'head_features/32'
+  python ${0%/*}/../../train.py -m resnet -b 128 --limitTrainBatches 0.25 -nh 8 -nl 1 --imageToFeature True --backboneFeatures 128 -ih 16 -f 'head_features/16'
 done
