877484a65f3334717bf9930f6e472e920e7be2f2
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-08-24 13:31:50 +0200

    Add final scripts

--- scripts/patches/final.sh
+++ scripts/patches/final.sh
@@ -1,12 +1,11 @@
-for i in {1..5}; do
-  echo "not implemented: run other versions first"
+for i in {1..3}; do
   # Users
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 1 -f 'final/user1'
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 2 -f 'final/user2'
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType user --crossEvalExclude 3 -f 'final/user3'
-  # # Scenes
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 1 -f 'final/scene1'
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 2 -f 'final/scene2'
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 3 -f 'final/scene3'
-  # python ${0%/*}/../../train.py -m patches -nh 1 -nl 1 --crossEvalType scene --crossEvalExclude 4 -f 'final/scene4'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType user --crossEvalExclude 1 -f 'final/user1'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType user --crossEvalExclude 2 -f 'final/user2'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType user --crossEvalExclude 3 -f 'final/user3'
+  # Scenes
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType scene --crossEvalExclude 1 -f 'final/scene1'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType scene --crossEvalExclude 2 -f 'final/scene2'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType scene --crossEvalExclude 3 -f 'final/scene3'
+  python ${0%/*}/../../train.py -m patches -nh 2 -nl 1 --imageToFeature True --backboneFeatures 16 -ih 128 --crossEvalType scene --crossEvalExclude 4 -f 'final/scene4'
 done
