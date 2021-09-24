a58425ad4ee942891320277863f954ae0197f783
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2021-09-24 10:06:36 +0200

    Update eval scripts

--- scripts/dino_all/delta.sh
+++ scripts/dino_all/delta.sh
@@ -1,4 +1,4 @@
 for i in {1..2}; do
-  python ${0%/*}/../../train.py -m dino --useAllImages True -nh 4 -nl 1 --delta True -f 'all/delta/true'
-  python ${0%/*}/../../train.py -m dino --useAllImages True -nh 4 -nl 1 --delta False -f 'all/delta/false'
+  python ${0%/*}/../../train.py -m dino --useAllImages True -nh 1 -nl 1 --delta True -f 'all/delta/true'
+  python ${0%/*}/../../train.py -m dino --useAllImages True -nh 1 -nl 1 --delta False -f 'all/delta/false'
 done
--- scripts/dino_all/loss.sh
+++ scripts/dino_all/loss.sh
@@ -1,4 +1,4 @@
 for i in {1..2}; do
-  python ${0%/*}/../../train.py -m dino --useAllImages True -l angular -nh 4 -nl 1 -f 'all/loss/angular'
-  python ${0%/*}/../../train.py -m dino --useAllImages True -l mse -nh 4 -nl 1 -f 'all/loss/mse'
+  python ${0%/*}/../../train.py -m dino --useAllImages True -l angular -nh 1 -nl 1 -f 'all/loss/angular'
+  python ${0%/*}/../../train.py -m dino --useAllImages True -l mse -nh 1 -nl 1 -f 'all/loss/mse'
 done
